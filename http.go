0d9d719c621b3a48717fb662b3f5690facab8937
booscaaa <boscardinvinicius@gmail.com>
2026-03-06 11:59:17 -0300

    :recycle: refactor(http): update method signatures to use exported types

--- http.go
+++ http.go
@@ -9,26 +9,26 @@ import (
 	"net/http"
 )
 
-func (l *locksmith) generateAccessToken(ctx context.Context, input accessTokenInput) (accessTokenOutput, error) {
+func (l *locksmith) generateAccessToken(ctx context.Context, input AccessTokenInput) (AccessTokenOutput, error) {
 	input.ClientID = l.clientID
 	input.ClientSecret = l.clientSecret
 	input.GrantType = "authorization_code"
 
 	jsonInput, err := json.Marshal(input)
 	if err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, l.baseUrl+"/api/oauth2/access-token", bytes.NewReader(jsonInput))
 	if err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 
 	defer resp.Body.Close()
@@ -36,36 +36,36 @@ func (l *locksmith) generateAccessToken(ctx context.Context, input accessTokenIn
 	if resp.StatusCode != http.StatusOK {
 		var errorResponse ApiError
 		if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
-			return accessTokenOutput{}, err
+			return AccessTokenOutput{}, err
 		}
-		return accessTokenOutput{}, errorResponse
+		return AccessTokenOutput{}, errorResponse
 	}
 
-	var output accessTokenOutput
+	var output AccessTokenOutput
 	if err := json.NewDecoder(resp.Body).Decode(&output); err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 	return output, nil
 }
 
-func (l *locksmith) generateRefreshToken(ctx context.Context, input refreshAccessTokenInput) (accessTokenOutput, error) {
+func (l *locksmith) generateRefreshToken(ctx context.Context, input RefreshAccessTokenInput) (AccessTokenOutput, error) {
 	input.GrantType = "refresh_token"
 
 	jsonInput, err := json.Marshal(input)
 	if err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, l.baseUrl+"/api/oauth2/refresh-token", bytes.NewReader(jsonInput))
 	if err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 
 	defer resp.Body.Close()
@@ -73,108 +73,108 @@ func (l *locksmith) generateRefreshToken(ctx context.Context, input refreshAcces
 	if resp.StatusCode != http.StatusOK {
 		var errorResponse ApiError
 		if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
-			return accessTokenOutput{}, err
+			return AccessTokenOutput{}, err
 		}
-		return accessTokenOutput{}, errorResponse
+		return AccessTokenOutput{}, errorResponse
 	}
 
-	var output accessTokenOutput
+	var output AccessTokenOutput
 	if err := json.NewDecoder(resp.Body).Decode(&output); err != nil {
-		return accessTokenOutput{}, err
+		return AccessTokenOutput{}, err
 	}
 	return output, nil
 }
 
-func (l *locksmith) createAccount(ctx context.Context, input accountInput) (accountOutput, error) {
+func (l *locksmith) createAccount(ctx context.Context, input AccountInput) (AccountOutput, error) {
 	jsonInput, err := json.Marshal(input)
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, l.baseUrl+"/api/accounts", bytes.NewReader(jsonInput))
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 
 	req.SetBasicAuth(l.clientID, l.clientSecret)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusBadRequest {
 		body, _ := io.ReadAll(resp.Body)
-		return accountOutput{}, errors.New(string(body))
+		return AccountOutput{}, errors.New(string(body))
 	}
 
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusNoContent {
 		var errorResponse ApiError
 		if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
-			return accountOutput{}, err
+			return AccountOutput{}, err
 		}
-		return accountOutput{}, errorResponse
+		return AccountOutput{}, errorResponse
 	}
 
-	var output accountOutput
+	var output AccountOutput
 	if err := json.NewDecoder(resp.Body).Decode(&output); err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 	return output, nil
 }
 
-func (l *locksmith) updateAccount(ctx context.Context, input accountInput) (accountOutput, error) {
+func (l *locksmith) updateAccount(ctx context.Context, input AccountInput) (AccountOutput, error) {
 	jsonInput, err := json.Marshal(input)
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPut, l.baseUrl+"/api/accounts/"+input.Id, bytes.NewReader(jsonInput))
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 
 	req.SetBasicAuth(l.clientID, l.clientSecret)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusBadRequest {
 		body, _ := io.ReadAll(resp.Body)
-		return accountOutput{}, errors.New(string(body))
+		return AccountOutput{}, errors.New(string(body))
 	}
 
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusNoContent {
 		var errorResponse ApiError
 		if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
-			return accountOutput{}, err
+			return AccountOutput{}, err
 		}
-		return accountOutput{}, errorResponse
+		return AccountOutput{}, errorResponse
 	}
 
-	var output accountOutput
+	var output AccountOutput
 	if err := json.NewDecoder(resp.Body).Decode(&output); err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 	return output, nil
 }
 
-func (l *locksmith) getAccountByID(ctx context.Context, id string) (accountOutput, error) {
+func (l *locksmith) getAccountByID(ctx context.Context, id string) (AccountOutput, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, l.baseUrl+"/api/accounts/"+id, nil)
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 	req.SetBasicAuth(l.clientID, l.clientSecret)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 
 	defer resp.Body.Close()
@@ -182,28 +182,28 @@ func (l *locksmith) getAccountByID(ctx context.Context, id string) (accountOutpu
 	if resp.StatusCode != http.StatusOK {
 		var errorResponse ApiError
 		if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
-			return accountOutput{}, err
+			return AccountOutput{}, err
 		}
-		return accountOutput{}, errorResponse
+		return AccountOutput{}, errorResponse
 	}
 
-	var output accountOutput
+	var output AccountOutput
 	if err := json.NewDecoder(resp.Body).Decode(&output); err != nil {
-		return accountOutput{}, err
+		return AccountOutput{}, err
 	}
 	return output, nil
 }
 
-func (l *locksmith) getPermissionsForUserInDomain(ctx context.Context, sub string, dom string) (permissionsOutput, error) {
+func (l *locksmith) getPermissionsForUserInDomain(ctx context.Context, sub string, dom string) (PermissionsOutput, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, l.baseUrl+"/api/acl/permissions/user/"+sub+"/domain/"+dom, nil)
 	if err != nil {
-		return permissionsOutput{}, err
+		return PermissionsOutput{}, err
 	}
 	req.SetBasicAuth(l.clientID, l.clientSecret)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return permissionsOutput{}, err
+		return PermissionsOutput{}, err
 	}
 
 	defer resp.Body.Close()
@@ -211,14 +211,14 @@ func (l *locksmith) getPermissionsForUserInDomain(ctx context.Context, sub strin
 	if resp.StatusCode != http.StatusOK {
 		var errorResponse ApiError
 		if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
-			return permissionsOutput{}, err
+			return PermissionsOutput{}, err
 		}
-		return permissionsOutput{}, errorResponse
+		return PermissionsOutput{}, errorResponse
 	}
 
-	var output permissionsOutput
+	var output PermissionsOutput
 	if err := json.NewDecoder(resp.Body).Decode(&output); err != nil {
-		return permissionsOutput{}, err
+		return PermissionsOutput{}, err
 	}
 	return output, nil
 }
